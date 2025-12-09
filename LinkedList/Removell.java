eca631db3718e752e1a60a8e1f5b8c3b4dc53afb
Veer24-tech <veerkushwaha212635@gmail.com>
2025-12-09 22:23:27 +0530

    RemoveFirst code

---
+++ LinkedList/Removell.java
@@ -0,0 +1,67 @@
+public class Removell{
+public class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public static Node head;
+public static Node tail;
+public static int size;
+ public void addFirst(int data){
+    //step1 creating a new node
+    Node newNode =new Node(data);
+    size++;
+    // if Linked list is empty
+    if(head==null){
+        head=tail=newNode;
+        return;
+    }
+    //step2 making newnode -> head
+    newNode.next=head;
+    head=newNode;
+ }
+ public int removeFirst() {
+    if (size == 0) {
+        System.out.println("Linked list is Empty");
+        return Integer.MIN_VALUE; // corrected
+    }
+
+    int val = head.data; // store value to return
+
+    if (size == 1) {
+        head = tail = null; // list becomes empty
+        size = 0;
+        return val;
+    }
+
+    head = head.next; // move head forward
+    size--; // decrement size correctly
+    return val;
+}
+
+ public void Display(){
+    Node temp=head;
+    while(temp!=null){
+        System.out.print(temp.data+"->");
+        temp=temp.next;
+    }
+    System.out.print("null");
+ }
+
+ public static void main(String args[]){
+    Removell obj=new Removell();
+    obj.addFirst(30);
+    obj.addFirst(20);
+    obj.addFirst(10);
+    obj.Display();
+    System.out.println("\nsize of linked list is "+obj.size);
+     obj.removeFirst();
+      obj.Display();
+      System.out.println("\nsize of linked list is "+obj.size);
+     
+ }
+
+}
