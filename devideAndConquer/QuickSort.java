f6cfd0bbeaae42a397f8ad7ef6437328b33d2862
Veer24-tech <rum2425442@ramauniversity.ac.in>
2026-02-25 23:05:51 +0530

    Quick Sort

--- devideAndConquer/QuickSort.java
+++ devideAndConquer/QuickSort.java
@@ -1,48 +1,49 @@
-public class QuickSort {
-
-    public static void printArr(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+public class QuickSort{
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
 
-    public static void quickSort(int arr[], int si, int ei) {
-        if (si >= ei) return;
 
-        int pidx = partition(arr, si, ei);
+    public static void quickSort(int arr[],int si ,int ei){
+    //base case
+        if(si>=ei){
+            return;
+        }
+        //partation
+        int pidx=partation(arr ,si ,ei);
+        quickSort(arr, si,pidx-1);
+        quickSort(arr,pidx+1,ei);
 
-        quickSort(arr, si, pidx - 1);
-        quickSort(arr, pidx + 1, ei);
     }
 
-    public static int partition(int arr[], int si, int ei) {
 
-        int pivot = arr[ei];
-        int i = si - 1;
-
-        for (int j = si; j < ei; j++) {
-            if (arr[j] < pivot) {
+    public static int partation(int arr[],int si,int ei){
+        int pivot=arr[ei];//last element as pivot
+        int i=si-1;//to find the correct index of pivot element
+        for(int j=si;j<ei;j++){
+            if(arr[j]<pivot){
                 i++;
-
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
             }
         }
-
+        //swap pivot element to correct index
         i++;
+        int temp=pivot;
+        arr[ei]=arr[i];
+        arr[i]=temp;
+        return i;//returning the correct index of pivot element
 
-        int temp = arr[ei];
-        arr[ei] = arr[i];
-        arr[i] = temp;
-
-        return i;
     }
-
-    public static void main(String args[]) {
-        int arr[] = {10, 3, 7, 4, 5, 0};
-        quickSort(arr, 0, arr.length - 1);
+    public static void main(String args[]){
+        int arr[]={10,7,9,3,2,5,-8,0};
+        quickSort(arr,0,arr.length-1);
         printArr(arr);
+
     }
 }
