51961277092bec381865fa9f647d80da92cd5ce2
Veer24-tech <rum2425442@ramauniversity.ac.in>
2026-06-11 21:01:24 +0530

    Otimized asteroidcollision

--- Stack/astroidCollision.java
+++ Stack/astroidCollision.java
@@ -1,57 +1,104 @@
-//brute force approach 
+// //brute force approach 
+// import java.util.*;
+
+// public class astroidCollision {
+
+//     public static int[] asteroidColl(int[] asteroids) {
+
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         for (int x : asteroids) {
+//             list.add(x);
+//         }                
+
+//         int i = 0;
+
+//         while (i < list.size() - 1) {
+
+//             int a = list.get(i);
+//             int b = list.get(i + 1);
+
+//             if (a > 0 && b < 0) {
+
+//                 if (a > -b) {
+//                     list.remove(i + 1);
+
+//                     if (i > 0) {
+//                         i--;
+//                     }
+
+//                 } else if (a < -b) {
+//                     list.remove(i);
+
+//                     if (i > 0) {
+//                         i--;
+//                     }
+
+//                 } else {
+//                     list.remove(i + 1);
+//                     list.remove(i);
+
+//                     if (i > 0) {
+//                         i--;
+//                     }
+//                 }
+
+//             } else {
+//                 i++;
+//             }
+//         }
+
+//         int[] ans = new int[list.size()];
+
+//         for (int j = 0; j < list.size(); j++) {
+//             ans[j] = list.get(j);
+//         }
+
+//         return ans;
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] asteroids = {3, 5, -6, 2, -1, 4};
+
+//         int[] result = asteroidColl(asteroids);
+
+//         System.out.println(Arrays.toString(result));
+//     }
+// }
+
+
+
 import java.util.*;
 
 public class astroidCollision {
 
-    public static int[] asteroidColl(int[] asteroids) {
+    public static int[] asteroidsColl(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
 
-        ArrayList<Integer> list = new ArrayList<>();
+        for (int a : asteroids) {
 
-        for (int x : asteroids) {
-            list.add(x);
-        }
+            while (!s.isEmpty() && s.peek() > 0 && a < 0
+                    && s.peek() < -a) {
+                s.pop();
+            }
 
-        int i = 0;
+            if (!s.isEmpty() && s.peek() > 0 && a < 0) {
 
-        while (i < list.size() - 1) {
-
-            int a = list.get(i);
-            int b = list.get(i + 1);
-
-            if (a > 0 && b < 0) {
-
-                if (a > -b) {
-                    list.remove(i + 1);
-
-                    if (i > 0) {
-                        i--;
-                    }
-
-                } else if (a < -b) {
-                    list.remove(i);
-
-                    if (i > 0) {
-                        i--;
-                    }
-
-                } else {
-                    list.remove(i + 1);
-                    list.remove(i);
-
-                    if (i > 0) {
-                        i--;
-                    }
+                if (s.peek() == -a) {
+                    s.pop();
                 }
 
+                // current asteroid destroyed
             } else {
-                i++;
+                s.push(a);
             }
         }
 
-        int[] ans = new int[list.size()];
+        int[] ans = new int[s.size()];
 
-        for (int j = 0; j < list.size(); j++) {
-            ans[j] = list.get(j);
+        for (int i = s.size() - 1; i >= 0; i--) {
+            ans[i] = s.pop();
         }
 
         return ans;
@@ -59,10 +106,12 @@ public class astroidCollision {
 
     public static void main(String[] args) {
 
-        int[] asteroids = {3, 5, -6, 2, -1, 4};
+        int[] asteroids = {3, 5, -6, 2, -1};
 
-        int[] result = asteroidColl(asteroids);
+        int[] result = asteroidsColl(asteroids);
 
-        System.out.println(Arrays.toString(result));
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
     }
 }
